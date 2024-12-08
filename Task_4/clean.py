9c4810b4c7b5a2521301aa8cff05ef268f4a02d0
Niduka Jayathunga <nidukajayathunga886@gmail.com>
2024-12-08 13:21:29 +0530

    reviews tokenized

--- Task_4/clean.py
+++ Task_4/clean.py
@@ -1,5 +1,22 @@
 import pandas as pd
 import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import word_tokenize
+
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('punkt')
+nltk.download('punkt_tab')
+
+stop_words = set(stopwords.words('english'))
+
+lemmatizer = WordNetLemmatizer()
+
+# no use of stemmer
+stemmer = PorterStemmer()
 
 wine_reviews = 'wine_reviews.csv'
 df = pd.read_csv(wine_reviews)
@@ -19,11 +36,30 @@ def to_lowercase(text):
 def remove_extra_whitespace(text):
   return re.sub(r'\s+', ' ', text).strip()
 
+# remove stop words (e.g., "and", "the", "is")
+def remove_stop_words(words):
+  return [word for word in words if word not in stop_words]
+
+# Lemmatization
+def lemmatize_text(words):
+  return [lemmatizer.lemmatize(word) for word in words]
+
+# stemming - not used
+def stem_text(words):
+  return [stemmer.stem(word) for word in words]
+
+# tokenize
+def tokenize_text(text):
+  return word_tokenize(text)
+
 def clean_reviews(text) :
   text = to_lowercase(text)
   text = remove_special_characters(text)
   text = remove_extra_whitespace(text)
-  return text
+  tokens = tokenize_text(text)
+  tokens = remove_stop_words(tokens)
+  tokens = lemmatize_text(tokens)
+  return tokens
 
 df['cleaned_reviews'] = df['review'].apply(clean_reviews)
 print(df.head())
