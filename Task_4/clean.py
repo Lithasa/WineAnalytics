b449c870723914f22ddcdb829a2da4bd8041f26f
Niduka Jayathunga <nidukajayathunga886@gmail.com>
2024-12-08 13:14:46 +0530

    Basic cleaning methods apply

--- Task_4/clean.py
+++ Task_4/clean.py
@@ -1,6 +1,29 @@
 import pandas as pd
+import re
 
 wine_reviews = 'wine_reviews.csv'
 df = pd.read_csv(wine_reviews)
 
+# drop missing values
+df = df.dropna(subset=['review'])
+
+# remove punctuation, emojis, and other symbols
+def remove_special_characters(text):
+  return re.sub(r'[^a-zA-Z0-9\s]', '', text)
+
+# convert all texts to lowercase
+def to_lowercase(text):
+  return text.lower()
+
+# remove whitespaces
+def remove_extra_whitespace(text):
+  return re.sub(r'\s+', ' ', text).strip()
+
+def clean_reviews(text) :
+  text = to_lowercase(text)
+  text = remove_special_characters(text)
+  text = remove_extra_whitespace(text)
+  return text
+
+df['cleaned_reviews'] = df['review'].apply(clean_reviews)
 print(df.head())
