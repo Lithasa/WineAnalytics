7811e157aa3080353dbab25242c8d0885711566a
sandaniW <sandaniwickramanayake@gmail.com>
2024-12-09 15:40:45 -0700

    final plot created

--- Task_3/plot.py
+++ Task_3/plot.py
@@ -6,3 +6,9 @@ df = pd.read_csv(labeled_reviews)
 
 grouped_df = df.groupby('talks_about').size().reset_index(name='frequency')
 print(grouped_df)
+
+fig, ax = plt.subplots()
+ax.pie(grouped_df['frequency'], labels=grouped_df['talks_about'], autopct='%1.1f%%')
+
+plt.title('Distribution of Review Categories')
+plt.show()
